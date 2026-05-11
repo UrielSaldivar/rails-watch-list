bac5d75d74d4e2111cfd324f21effca7fc45775d
Uriel Saldivar <uriel.profesional@gmail.com>
2026-05-10 21:42:38 -0600

    Configuración finalizada

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+// Una vez agregadas las gemas en Gemfile del Sprockets y creada la carpeta config + el archivo manifest.js, se agrega lo siguiente:
+
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+
+// Después de agregar Bootstrap JS
+
+//= link popper.js
+//= link bootstrap.min.js
